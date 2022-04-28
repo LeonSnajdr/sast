5e84365ae46e11f4c76f801b42148add77967ee0
lsnajdr <lsnajdr@samhammer.de>
2022-04-28 13:03:23 +0200

    First working version

--- source/main.ps1
+++ source/main.ps1
@@ -1,10 +1,51 @@
 # Include
 . .\libs\QuietusPlus-WriteMenu\Write-Menu.ps1
 
-$menuReturn = Write-Menu -Title 'Sam.Start Main menu' -Entries @(
-    'asimov'
-    'ssp'
-    'metis'
-)
+Get-Content "./config.txt" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+$ReposPath = $h.Get_Item("ReposPath")
 
-& .\projects\$menuReturn + '.ps1'
+function MainMenu {
+    $projectName = Write-Menu -Title "Sam.Start (c)lsnajdr" -Entries @(
+        'Asimov'
+        'SelfServicePortal'
+        'Metis'
+    )
+
+    DrawProjectMenu ($projectName)
+}
+
+function DrawProjectMenu($project) {
+
+    switch ($project) {
+        "Asimov" { 
+            $menuReturn = Write-Menu -Title "($ReposPath/$project) [INSERT] Select everything [SPACE] Select / Deselect [ENTER] Start selected projects" -MultiSelect -Entries @(
+                'AgentChat'
+                'Admin'
+                'Bot'
+                'Dispatcher'
+                'SocketServer'
+            )
+
+            foreach ($service in $menuReturn) {
+
+                Write-Output "Starting $service..."
+                Timeout /T 2
+
+                if ($service -eq 'AgentChat') {
+                    wt -w Asimov nt -p "Windows PowerShell" --title "$service" -d "$ReposPath\asimov\Sources\App.$service" cmd /k "yarn install && yarn serve" 
+                    continue
+                }
+
+                Write-Host $service
+                wt -w Asimov nt -p "Windows PowerShell" --title "$service" -d "$ReposPath\asimov\Sources\Asimov.$service" cmd /k "dotnet run" 
+            }
+        }
+        Default {
+            Write-Host "No matching project selected"
+            MainMenu
+        }
+    }
+    
+}
+
+MainMenu
