b3843a4678ca191920e265f0d920d5c8a45814a7
lsnajdr <lsnajdr@samhammer.de>
2022-07-08 11:48:57 +0200

    Add file start support

--- source/main.ps1
+++ source/main.ps1
@@ -1,3 +1,9 @@
+Param(
+    [parameter(Mandatory = $false)]
+    [alias("f")]
+    [string] $filePath
+)
+
 # Include
 . .\libs\QuietusPlus-WriteMenu\Write-Menu.ps1
 
@@ -14,56 +20,25 @@ function MainMenu {
         'Configuration'
     )
 
-    DrawProjectMenu ($projectName)
+    DrawProjectMenu $projectName
 }
 
 function DrawProjectMenu($project) {
 
     switch ($project) {
         "Asimov" { 
-            $menuReturn = Write-Menu -Title "($ReposPath/$project) [INSERT] Select everything [SPACE] Select / Deselect [ENTER] Start selected projects" -MultiSelect -Entries @(
-                'AgentChat'
-                'Admin'
-                'Bot'
-                'Dispatcher'
-                'SocketServer'
+            $services = Write-Menu -Title "($ReposPath/$project) [INSERT] Select everything [SPACE] Select / Deselect [ENTER] Start selected projects" -MultiSelect -Entries @(
+                'App.AgentChat'
+                'Asimov.Admin'
+                'Asimov.Bot'
+                'Asimov.Dispatcher'
+                'Asimov.SocketServer'
+                'Asimov.TaskScheduler'
             )
 
-            if ($CompileSolution) {
-                Write-Host "Compiling solution..."
-                dotnet build "$ReposPath\asimov\Sources" | Out-Null
-            }
-
-            foreach ($service in $menuReturn) {
-
-                $percent = [Math]::Round((($menuReturn.IndexOf($service)) / $menuReturn.Count) * 100, 0)
-
-                $status = "Starting $service..."
-                if ($CompileProject) {
-                    $status = "Compiling & Starting $service..."
-                }
-
-                Write-Progress -Activity "Starting selected Projects" -Status $status -PercentComplete  $percent
-
-                if ($service -eq 'AgentChat') {
-                    wt -w Asimov nt -p "Windows PowerShell" --title "$service" -d "$ReposPath\asimov\Sources\App.$service" cmd /k "yarn install && yarn serve" 
-                }
-                else {
-                    if ($CompileProject) {
-                        dotnet build "$ReposPath\asimov\Sources\Asimov.$service" | Out-Null
-                    }
-    
-                    wt -w Asimov nt -p "Windows PowerShell" --title "$service" -d "$ReposPath\asimov\Sources\Asimov.$service" cmd /k "dotnet run"
-                }
-
-                if ($menuReturn.IndexOf($service) -eq 0 -and !$CompileProject) {
-                    # Wait two seconds before opening the next projects to open in same terminal window 
-                    Timeout /T 2 | Out-Null
-                }
-            }
+            StartProject 'Asimov' 'asimov\Sources' $services @('App.AgentChat')
 
             Read-Host "Press [ENTER] to get back to the main menu"
-            MainMenu
         }
 
         "Configuration" {
@@ -84,4 +59,58 @@ function DrawProjectMenu($project) {
     
 }
 
-MainMenu
+function StartProject ($projectName, $projectPath, $services, $apps) {
+
+    Clear-Host
+    Write-Host $projectPath
+
+    if ($CompileSolution) {
+        Write-Host "Compiling solution..."
+        dotnet build "$ReposPath\$projectPath" | Out-Null
+    }
+
+    foreach ($service in $services) {
+
+        $percent = [Math]::Round((($services.IndexOf($service)) / $services.Count) * 100, 0)
+
+        $status = "Starting $service..."
+        if ($CompileProject) {
+            $status = "Compiling & Starting $service..."
+        }
+
+        Write-Progress -Activity "Starting selected Projects" -Status $status -PercentComplete  $percent
+
+        if ($apps -contains $service) {
+            wt -w $projectName nt -p "Windows PowerShell" --title "$service" -d "$ReposPath\$projectPath\$service" cmd /k "yarn install && yarn serve" 
+        }
+        else {
+            if ($CompileProject) {
+                dotnet build "$ReposPath\$projectPath\$service" | Out-Null
+            }
+
+            wt -w $projectName nt -p "Windows PowerShell" --title "$service" -d "$ReposPath\$projectPath\$service" cmd /k "dotnet run"
+        }
+
+        if ($services.IndexOf($service) -eq 0 -and !$CompileProject) {
+            # Wait two seconds before opening the next projects to open in same terminal window 
+            Timeout /T 2 | Out-Null
+        }
+    }
+}
+
+function StartWithFile($filePath) {
+    Get-Content $filePath | foreach-object -begin { $h = @{} } -process { $k = [regex]::split($_, '='); if (($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+    $projectName = $h.Get_Item("projectName")
+    $projectPath = $h.Get_Item("projectPath")
+    $services = $h.Get_Item("services") -split ', '
+    $apps = $h.Get_Item("apps") -split ', '
+
+    StartProject $ProjectName $projectPath $services $apps
+}
+
+if ([string]::IsNullOrWhiteSpace($filePath)) {
+    MainMenu
+}
+else {
+    StartWithFile $filePath
+}
