33f3ba872aed7a830f3abb54db928ffc26874839
lsnajdr <lsnajdr@samhammer.de>
2022-07-11 09:10:15 +0200

    Add readme

--- source/main.ps1
+++ source/main.ps1
@@ -8,9 +8,9 @@ Param(
 . .\libs\QuietusPlus-WriteMenu\Write-Menu.ps1
 
 Get-Content "./config.txt" | foreach-object -begin { $h = @{} } -process { $k = [regex]::split($_, '='); if (($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
-$ReposPath = $h.Get_Item("ReposPath")
-$CompileSolution = [System.Convert]::ToBoolean($h.Get_Item("CompileSolution"))
-$CompileProject = [System.Convert]::ToBoolean($h.Get_Item("CompileProject"))
+$reposPath = $h.Get_Item("reposPath")
+$compileSolution = [System.Convert]::ToBoolean($h.Get_Item("compileSolution"))
+$compileProject = [System.Convert]::ToBoolean($h.Get_Item("compileProject"))
 
 function PrintLogo {
     Clear-Host
@@ -39,7 +39,7 @@ function DrawProjectMenu($project) {
 
     switch ($project) {
         "Asimov" { 
-            $services = Write-Menu -Title "($ReposPath/$project) [INSERT] Select everything [SPACE] Select / Deselect [ENTER] Start selected projects" -MultiSelect -Entries @(
+            $services = Write-Menu -Title "($reposPath/$project) [INSERT] Select everything [SPACE] Select / Deselect [ENTER] Start selected projects" -MultiSelect -Entries @(
                 'App.AgentChat'
                 'Asimov.Admin'
                 'Asimov.Bot'
@@ -55,9 +55,9 @@ function DrawProjectMenu($project) {
 
         "Configuration" {
             Write-Host "Configuration"
-            Write-Host "Repos path: $ReposPath"
-            Write-Host "CompileSolution: $CompileSolution"
-            Write-Host "CompileProject: $CompileProject"
+            Write-Host "Repos path: $reposPath"
+            Write-Host "compileSolution: $compileSolution"
+            Write-Host "compileProject: $compileProject"
             Read-Host "Press [ENTER] to get back to the main menu"
             MainMenu
         }
@@ -76,9 +76,9 @@ function StartProject ($projectName, $projectPath, $services, $apps) {
     PrintLogo
     Write-Host $projectPath
 
-    if ($CompileSolution) {
+    if ($compileSolution) {
         Write-Host "Compiling solution..."
-        dotnet build "$ReposPath\$projectPath" | Out-Null
+        dotnet build "$reposPath\$projectPath" | Out-Null
     }
 
     foreach ($service in $services) {
@@ -86,24 +86,24 @@ function StartProject ($projectName, $projectPath, $services, $apps) {
         $percent = [Math]::Round((($services.IndexOf($service)) / $services.Count) * 100, 0)
 
         $status = "Starting $service..."
-        if ($CompileProject) {
+        if ($compileProject) {
             $status = "Compiling & Starting $service..."
         }
 
         Write-Progress -Activity "Starting selected Projects" -Status $status -PercentComplete  $percent
 
         if ($apps -contains $service) {
-            wt -w $projectName nt -p "Windows PowerShell" --title "$service" -d "$ReposPath\$projectPath\$service" cmd /k "yarn install && yarn serve" 
+            wt -w $projectName nt -p "Windows PowerShell" --title "$service" -d "$reposPath\$projectPath\$service" cmd /k "yarn install && yarn serve" 
         }
         else {
-            if ($CompileProject) {
-                dotnet build "$ReposPath\$projectPath\$service" | Out-Null
+            if ($compileProject) {
+                dotnet build "$reposPath\$projectPath\$service" | Out-Null
             }
 
-            wt -w $projectName nt -p "Windows PowerShell" --title "$service" -d "$ReposPath\$projectPath\$service" cmd /k "dotnet run"
+            wt -w $projectName nt -p "Windows PowerShell" --title "$service" -d "$reposPath\$projectPath\$service" cmd /k "dotnet run"
         }
 
-        if ($services.IndexOf($service) -eq 0 -and !$CompileProject) {
+        if ($services.IndexOf($service) -eq 0 -and !$compileProject) {
             # Wait two seconds before opening the next projects to open in same terminal window 
             Timeout /T 2 | Out-Null
         }
