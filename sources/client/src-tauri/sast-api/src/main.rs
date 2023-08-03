6e3483159c93efbaeef48a5cadfa5428019500d6
LeonSnajdr <mail@leonsnajdr.de>
2023-08-03 18:04:20 +0200

    add prisma dev

--- sources/client/src-tauri/sast-api/src/main.rs
+++ sources/client/src-tauri/sast-api/src/main.rs
@@ -1,8 +1,49 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
+#[allow(warnings, unused)]
+mod prisma;
+
+use prisma::*;
+use serde::Deserialize;
+use specta::{collect_types, Type};
+use std::sync::Arc;
+use tauri::State;
+use tauri_specta::ts;
+
+type DbState<'a> = State<'a, Arc<PrismaClient>>;
+
+#[tauri::command]
+#[specta::specta]
+async fn greet(db: DbState<'_>, create: CreateProjectData) -> Result<String, String> {
+  let result = "Test".to_string();
+
+  return Ok(result);
+}
+
+#[tauri::command]
+#[specta::specta]
+async fn create_project(db: DbState<'_>, create: CreateProjectData) -> Result<project::Data, String> {
+  let data: project::Data = db.project().create(create.name,  vec![]).exec().await.unwrap();
+
+  return Ok(data);
+}
+
+#[derive(Deserialize, Type)]
+struct CreateProjectData {
+  name: String,
+}
+
+#[tokio::main]
+async fn main() {
+  let db = PrismaClient::_builder().build().await.unwrap();
+
+  #[cfg(debug_assertions)]
+  ts::export(collect_types![greet, create_project], "../src/bindings.ts").unwrap();
+
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+      .invoke_handler(tauri::generate_handler![greet])
+      .manage(Arc::new(db))
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
--- sources/client/src/App.vue
+++ sources/client/src/App.vue
@@ -1,9 +1,23 @@
 <template>
     <div class="card flex justify-content-center">
-        <Button label="Check" icon="pi pi-check" />
+        <Button label="Check" icon="pi pi-check" @click="click" />
+        <Button label="Create" @click="create" />
+        <h2>{{ result }}</h2>
     </div>
 </template>
 
 <script setup lang="ts">
 import Button from "primevue/button";
+import * as commands from "./bindings";
+import { ref } from "vue";
+
+const result = ref<string>();
+
+async function click(): Promise<void> {
+    // result.value = await commands.greet({ name: "Keck" });
+}
+
+async function create(): Promise<void> {
+    await commands.createProject({ name: "keckw" });
+}
 </script>
