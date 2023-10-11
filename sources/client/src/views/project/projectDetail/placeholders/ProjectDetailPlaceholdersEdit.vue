4338beb380fae4d5462cf764eb67f5c39c82b307
LeonSnajdr <94352142+LeonSnajdr@users.noreply.github.com>
2023-10-11 20:49:09 +0200

    Better usage of placeholders

--- sources/client/src/views/project/projectDetail/placeholders/ProjectDetailPlaceholdersEdit.vue
+++ sources/client/src/views/project/projectDetail/placeholders/ProjectDetailPlaceholdersEdit.vue
@@ -3,29 +3,39 @@
         <v-card-title>
             {{ $t("projectDetailPlaceholdersEdit.title") }}
             <v-spacer />
-            <v-btn @click="inEditMode = false" icon="mdi-close" size="small" />
+            <v-btn @click="inEditMode = false" variant="plain">
+                <v-icon icon="mdi-close" />
+            </v-btn>
         </v-card-title>
         <v-card-text>
-            <v-row v-for="placeholder in project.placeholders" :key="placeholder.id">
-                <v-col cols="11">
-                    <v-text-field :label="placeholder.name" v-model="placeholder.value" @update:modelValue="placeholderChanged(placeholder)"></v-text-field>
-                </v-col>
-                <v-col cols="1">
-                    <v-btn @click="deletePlaceholder(placeholder)" variant="plain" block>
-                        <v-icon icon="mdi-delete" />
-                    </v-btn>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="11">
-                    <v-text-field v-model="placeholderName" :placeholder="$t('projectDetailPlaceholdersEdit.input.name')"></v-text-field>
-                </v-col>
-                <v-col cols="1">
-                    <v-btn @click="createPlaceholder" variant="plain" block>
-                        <v-icon icon="mdi-plus" />
-                    </v-btn>
-                </v-col>
-            </v-row>
+            <div v-for="placeholder in project.placeholders" :key="placeholder.id" class="d-flex mb-4">
+                <v-text-field :label="placeholder.name" v-model="placeholder.value" @update:modelValue="placeholderChanged(placeholder)"></v-text-field>
+                <v-btn @click="deletePlaceholder(placeholder)" variant="plain">
+                    <v-icon icon="mdi-delete" />
+                </v-btn>
+            </div>
+
+            <v-form v-model="valid" class="d-flex">
+                <v-row>
+                    <v-col>
+                        <v-text-field
+                            v-model="placeholderName"
+                            :placeholder="$t('projectDetailPlaceholdersEdit.input.name')"
+                            :rules="[required($t('projectDetailPlaceholdersEdit.input.name.required'))]"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                            v-model="placeholderValue"
+                            :placeholder="$t('projectDetailPlaceholdersEdit.input.value')"
+                            :rules="[required($t('projectDetailPlaceholdersEdit.input.value.required'))]"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-btn @click="createPlaceholder" :disabled="!valid" variant="plain">
+                    <v-icon icon="mdi-plus" />
+                </v-btn>
+            </v-form>
         </v-card-text>
     </v-card>
 </template>
@@ -33,6 +43,7 @@
 <script setup lang="ts">
 import type { CreatePlaceholderContract, Placeholder, UpdatePlaceholderContract } from "@/bindings";
 import * as commands from "@/bindings";
+import { required } from "@/rules";
 import { remove } from "lodash";
 
 const { inEditMode } = defineModels<{
@@ -44,7 +55,9 @@ const projectStore = useProjectStore();
 
 const { project } = storeToRefs(projectStore);
 const loading = ref(false);
+const valid = ref(false);
 const placeholderName = ref("");
+const placeholderValue = ref("");
 
 const createPlaceholder = async () => {
     loading.value = true;
@@ -52,7 +65,7 @@ const createPlaceholder = async () => {
     const createContract: CreatePlaceholderContract = {
         project_id: project.value.id,
         name: placeholderName.value,
-        value: ""
+        value: placeholderValue.value
     };
 
     try {
@@ -65,6 +78,8 @@ const createPlaceholder = async () => {
         notify.error("projectDetailPlaceholdersEdit.create.error");
     } finally {
         placeholderName.value = "";
+        placeholderValue.value = "";
+
         loading.value = false;
     }
 };
