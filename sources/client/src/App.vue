c1cdf038ff85dfaf595a0a05a95fb6ecc2e3763d
LeonSnajdr <mail@leonsnajdr.de>
2023-10-11 16:10:21 +0200

    Fix layout

--- sources/client/src/App.vue
+++ sources/client/src/App.vue
@@ -16,3 +16,23 @@ import Notification from "@/components/Notification.vue";
 
 const route = useRoute();
 </script>
+
+<style lang="scss">
+@use "@/styles/settings";
+@import "@/styles/global";
+
+$container-padding: settings.$container-padding-x;
+
+.v-main {
+    margin-left: $container-padding;
+    width: calc(100% - $container-padding) !important;
+
+    .v-navigation-drawer {
+        &--left {
+            margin-left: $container-padding;
+            margin-top: $container-padding;
+            height: calc(100% - $container-padding * 2) !important;
+        }
+    }
+}
+</style>
--- sources/client/src/layouts/ProjectLayout.vue
+++ sources/client/src/layouts/ProjectLayout.vue
@@ -1,28 +1,10 @@
 <template>
     <ProjectList />
-    <slot></slot>
+    <v-container class="h-100" fluid>
+        <slot></slot>
+    </v-container>
 </template>
 
 <script setup lang="ts">
 import ProjectList from "@/views/project/ProjectList.vue";
 </script>
-
-<style lang="scss">
-@use "@/styles/settings";
-@import "@/styles/global";
-
-$container-padding: settings.$container-padding-x;
-
-.v-main {
-    margin-left: $container-padding;
-    width: calc(100% - $container-padding) !important;
-
-    .v-navigation-drawer {
-        &--left {
-            margin-left: $container-padding;
-            margin-top: $container-padding;
-            height: calc(100% - $container-padding * 2) !important;
-        }
-    }
-}
-</style>
