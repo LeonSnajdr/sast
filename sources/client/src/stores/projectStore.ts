8708476df48d3cb351e3b1e385e2a60b0c15fbef
LeonSnajdr <mail@leonsnajdr.de>
2023-10-12 10:53:30 +0200

    Small refactorings

--- sources/client/src/stores/projectStore.ts
+++ sources/client/src/stores/projectStore.ts
@@ -4,35 +4,29 @@ import { ref } from "vue";
 import * as commands from "@/bindings";
 import { useNotificationStore } from "./notificationStore";
 
-export const useProjectStore = defineStore(
-    "project",
-    () => {
-        const notify = useNotificationStore();
+export const useProjectStore = defineStore("project", () => {
+    const notify = useNotificationStore();
 
-        const listProjects = ref<ListProjectContract[]>([]);
-        const project = ref<FullProjectContract>();
+    const listProjects = ref<ListProjectContract[]>([]);
+    const project = ref<FullProjectContract>();
 
-        const loadListProjects = async () => {
-            try {
-                listProjects.value = await commands.getListProjects();
-            } catch (error) {
-                console.error("Loading list projects failed", error);
-                notify.error("projectStore.projectList.load.failed");
-            }
-        };
+    const loadListProjects = async () => {
+        try {
+            listProjects.value = await commands.getListProjects();
+        } catch (error) {
+            console.error("Loading list projects failed", error);
+            notify.error("projectStore.projectList.load.failed");
+        }
+    };
 
-        const loadProject = async (projectId: string) => {
-            try {
-                project.value = await commands.getFullProject(projectId);
-            } catch (error) {
-                console.error("Loading project failed", error);
-                notify.error("projectStore.project.load.failed");
-            }
-        };
+    const loadProject = async (projectId: string) => {
+        try {
+            project.value = await commands.getFullProject(projectId);
+        } catch (error) {
+            console.error("Loading project failed", error);
+            notify.error("projectStore.project.load.failed");
+        }
+    };
 
-        return { listProjects, project, loadProject, loadListProjects };
-    },
-    {
-        persist: { storage: localStorage }
-    }
-);
+    return { listProjects, project, loadProject, loadListProjects };
+});
