d4d4aedd1cd465ab7c6922f0b03c687382f76564
LeonSnajdr <mail@leonsnajdr.de>
2023-11-03 16:12:59 +0100

    langauge select

--- sources/client/src/views/settings/SettingsGeneral.vue
+++ sources/client/src/views/settings/SettingsGeneral.vue
@@ -1,5 +1,16 @@
 <template>
-    <h2>Settings</h2>
+    <v-card>
+        <v-card-title>Settings</v-card-title>
+        <v-card-text>
+            <v-row-single>
+                <v-select label="Sprache" :items="availableLocales"> </v-select>
+            </v-row-single>
+        </v-card-text>
+    </v-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const { availableLocales } = useI18n();
+</script>
