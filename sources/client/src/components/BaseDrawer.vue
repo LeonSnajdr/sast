03063275fe8b06b3cd635832ed7ec696dcf04e54
LeonSnajdr <mail@leonsnajdr.de>
2024-02-02 11:57:03 +0100

    Move drawer design in own scss file

--- sources/client/src/components/BaseDrawer.vue
+++ sources/client/src/components/BaseDrawer.vue
@@ -25,30 +25,6 @@ import { getVersion } from "@tauri-apps/api/app";
 import { computedAsync } from "@vueuse/core";
 
 const version = computedAsync(async () => {
-    console.log("get verison");
-
     return await getVersion();
 }, "");
 </script>
-
-<style lang="scss" scoped>
-.v-list-item {
-    :deep(.v-list-item__overlay) {
-        opacity: 1;
-        background-color: transparent;
-    }
-
-    &:hover {
-        :deep(.v-list-item__overlay) {
-            background-color: rgba(var(--v-theme-secondary), 0.2);
-        }
-    }
-
-    &--active {
-        :deep(.v-list-item__overlay) {
-            background-color: rgba(var(--v-theme-secondary), 0.2);
-            border-left: 3px solid rgb(var(--v-theme-primary));
-        }
-    }
-}
-</style>
