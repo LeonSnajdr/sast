f70f87c55d42968f101c7f1db39fd4377df41b63
LeonSnajdr <leonsnajdr@gmail.com>
2024-10-18 13:58:41 +0200

    Add tauri

--- sources/app/nuxt.config.ts
+++ sources/app/nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
+    ssr: false,
+    devServer: { host: process.env.TAURI_DEV_HOST || "localhost" },
+    vite: {
+        clearScreen: false,
+        envPrefix: ["VITE_", "TAURI_"],
+        server: {
+            strictPort: true
+        }
+    },
     modules: ["@nuxt/eslint", "vuetify-nuxt-module", "@nuxtjs/i18n"],
     i18n: {
         locales: [
