6d98724e886878ccf74d0665aec0971237af3378
LeonSnajdr <leonsnajdr@gmail.com>
2024-11-15 10:59:02 +0100

    Adjust eslint config

--- sources/app/eslint.config.mjs
+++ sources/app/eslint.config.mjs
@@ -1,38 +1,44 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt().override("nuxt/vue/rules", {
-    rules: {
-        "vue/multi-word-component-names": "off",
-        "vue/v-on-event-hyphenation": ["error", "never"],
-        "vue/attribute-hyphenation": ["error", "never"],
-        "vue/component-name-in-template-casing": ["error", "PascalCase", { registeredComponentsOnly: false }],
-        "vue/component-api-style": ["error", ["script-setup"]],
-        "vue/define-macros-order": [
-            "error",
-            {
-                order: ["defineEmits", "defineProps", "defineModel"]
-            }
-        ],
-        "vue/attributes-order": [
-            "error",
-            {
-                order: [
-                    "DEFINITION",
-                    "LIST_RENDERING",
-                    "CONDITIONALS",
-                    "RENDER_MODIFIERS",
-                    "GLOBAL",
-                    ["UNIQUE", "SLOT"],
-                    "TWO_WAY_BINDING",
-                    "EVENTS",
-                    "ATTR_DYNAMIC",
-                    "ATTR_STATIC",
-                    "ATTR_SHORTHAND_BOOL",
-                    "OTHER_DIRECTIVES",
-                    "CONTENT"
-                ],
-                alphabetical: true
-            }
-        ]
-    }
-});
+export default withNuxt()
+    .override("nuxt/vue/rules", {
+        rules: {
+            "vue/multi-word-component-names": "off",
+            "vue/v-on-event-hyphenation": ["error", "never"],
+            "vue/attribute-hyphenation": ["error", "never"],
+            "vue/component-name-in-template-casing": ["error", "PascalCase", { registeredComponentsOnly: false }],
+            "vue/component-api-style": ["error", ["script-setup"]],
+            "vue/define-macros-order": [
+                "error",
+                {
+                    order: ["defineEmits", "defineProps", "defineModel"]
+                }
+            ],
+            "vue/attributes-order": [
+                "error",
+                {
+                    order: [
+                        "DEFINITION",
+                        "LIST_RENDERING",
+                        "CONDITIONALS",
+                        "RENDER_MODIFIERS",
+                        "GLOBAL",
+                        ["UNIQUE", "SLOT"],
+                        "TWO_WAY_BINDING",
+                        "EVENTS",
+                        "ATTR_DYNAMIC",
+                        "ATTR_STATIC",
+                        "ATTR_SHORTHAND_BOOL",
+                        "OTHER_DIRECTIVES",
+                        "CONTENT"
+                    ],
+                    alphabetical: true
+                }
+            ]
+        }
+    })
+    .override("nuxt/vue/single-root", {
+        rules: {
+            "vue/no-multiple-template-root": "off"
+        }
+    });
