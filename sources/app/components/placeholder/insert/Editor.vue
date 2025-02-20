02148381832d4d8d7fee52b649b93cd6299989b4
LeonSnajdr <mail@leonsnajdr.de>
2025-02-20 15:57:02 +0100

    Add label that field supports placeholders

--- sources/app/components/placeholder/insert/Editor.vue
+++ sources/app/components/placeholder/insert/Editor.vue
@@ -1,6 +1,10 @@
 <template>
     <VField :id class="pa-2 mb-2" variant="outlined" active>
         <input v-show="false" :id="id" />
+        <template #label>
+            <span>{{ label }}</span>
+            <VIcon class="ml-1" icon="mdi-label" size="12" />
+        </template>
         <VueTextInsertEditor v-model="tiles" :editorOptions="options" />
     </VField>
 </template>
@@ -9,6 +13,10 @@
 import { PlaceholderInsertChip, PlaceholderInsertMenu } from "#components";
 import type { EditorOptions } from "vue-text-insert";
 import { VueTextInsertEditor } from "vue-text-insert";
+
+defineProps<{
+    label: string;
+}>();
 
 const tiles = defineModel<PlaceholderInsertTileContract[]>({ required: true });
 
